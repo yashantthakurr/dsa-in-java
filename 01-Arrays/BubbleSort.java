2b4b850ab2b48d6ce87d191b461fe1bfb7a26c22
Yashant Thakur <yashant.thakur2007@gmail.com>
2026-02-15 12:37:40 +0530

    Add files via upload

---
+++ 01-Arrays/BubbleSort.java
@@ -0,0 +1,38 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {78, 89, 47, 45, 76, 15, 24, 86, 35};
+        System.out.println("Unsorted Array: ");
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+
+        bubbleSort(array);
+
+        System.out.println("\nSorted Array: ");
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    private static void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
