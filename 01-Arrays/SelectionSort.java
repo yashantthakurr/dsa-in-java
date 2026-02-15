d627d97e112cae0843542b3458442097ee520400
Yashant Thakur <yashant.thakur2007@gmail.com>
2026-02-15 13:27:44 +0530

    Add files via upload

---
+++ 01-Arrays/SelectionSort.java
@@ -0,0 +1,40 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {78, 89, 47, 45, 76, 15, 24, 86, 35};
+        System.out.println("Unsorted Array: ");
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+
+        selectionSort(array);
+
+        System.out.println("\nSorted Array: ");
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    private static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[min] > a[j]) {
+                    min = j;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+        }
+    }
+}
